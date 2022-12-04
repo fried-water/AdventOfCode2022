e048f69794ade27c82dc77ab566a9ca78ad9e6ae
Leslie Barron <lesliebarron1@gmail.com>
2022-12-04 02:43:15 -0500

    refactor with collect_tuple(), and multi split

--- src/day4.rs
+++ src/day4.rs
@@ -1,32 +1,22 @@
-fn split2(s: &str, c: char) -> (&str, &str) {
-    let mut v = s.split(c);
-    (v.next().unwrap(), v.next().unwrap())
-}
+use itertools::Itertools;
 
-fn range((x, y): (&str, &str)) -> (i32, i32) {
-    (x.parse().unwrap(), y.parse().unwrap())
-}
-
-fn contains_range((f1, l1): (i32, i32), (f2, l2): (i32, i32)) -> bool {
-    f1 >= f2 && l1 <= l2
-}
-
-fn contains((f, l): (i32, i32), x: i32) -> bool {
-    x >= f && x <= l
+fn parse(line: &str) -> (i32, i32, i32, i32) {
+    line.split([',', '-'])
+        .map(|x| x.parse().unwrap())
+        .collect_tuple::<(_, _, _, _)>()
+        .unwrap()
 }
 
 pub fn part1(v: Vec<String>) -> usize {
     v.iter()
-        .map(|line| split2(line, ','))
-        .map(|(a, b)| (range(split2(a, '-')), range(split2(b, '-'))))
-        .filter(|(a, b)| contains_range(*a, *b) || contains_range(*b, *a))
+        .map(|l| parse(l))
+        .filter(|(a, b, x, y)| (a >= x && b <= y) || (x >= a && y <= b))
         .count()
 }
 
 pub fn part2(v: Vec<String>) -> usize {
     v.iter()
-        .map(|line| split2(line, ','))
-        .map(|(a, b)| (range(split2(a, '-')), range(split2(b, '-'))))
-        .filter(|(a, b)| contains(*a, b.0) || contains(*b, a.0))
+        .map(|l| parse(l))
+        .filter(|(a, b, x, y)| (x >= a && x <= b) || (a >= x && a <= y))
         .count()
 }
