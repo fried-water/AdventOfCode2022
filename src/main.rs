2a9c00aef3e97380f53bd5b9bcaf915e006ae68f
Leslie Barron <lesliebarron1@gmail.com>
2022-12-06 02:20:13 -0500

    day6

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ pub mod day2;
 pub mod day3;
 pub mod day4;
 pub mod day5;
+pub mod day6;
 
 type ProblemFn = Box<dyn Fn(Vec<String>) -> String>;
 
@@ -19,6 +20,7 @@ fn problems() -> Vec<Vec<ProblemFn>> {
         vec![string_wrap(day3::part1), string_wrap(day3::part2)],
         vec![string_wrap(day4::part1), string_wrap(day4::part2)],
         vec![string_wrap(day5::part1), string_wrap(day5::part2)],
+        vec![string_wrap(day6::part1), string_wrap(day6::part2)],
     ]
 }
 
